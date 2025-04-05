351a2f864c83faae25460c76ac0d157c72af14de
skrungly <kingsleymcdonald@outlook.com>
2025-04-05 05:34:11 +0100

    further minor improvements to gallery

--- app/static/script.js
+++ app/static/script.js
@@ -1,16 +1,17 @@
+function blankViewer() {
+    document.getElementById("gallery_fullsize").setAttribute("src", "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=")
+}
+
 function showViewer(clicked_image) {
     img_name = clicked_image.getAttribute("src").split("/").pop()
     img_path = "/static/gallery/" + img_name
 
-    // only show the viewer once the image is loaded
-    clicked_image.addEventListener("load", function() {
-        document.getElementById("gallery_viewer").classList.remove("gallery__viewer--hidden")
-    })
-
+    blankViewer()
     document.getElementById("gallery_fullsize").setAttribute("src", img_path)
     document.getElementById("gallery_original").setAttribute("href", img_path)
     document.getElementById("gallery_caption").innerText = clicked_image.dataset.metadata
-    clicked_image.setAttribute("src", img_path)
+
+    document.getElementById("gallery_viewer").classList.remove("gallery__viewer--hidden")
 }
 
 window.addEventListener("load", function () {
@@ -19,8 +20,9 @@ window.addEventListener("load", function () {
     // we want the photo viewer to close when clicking outside any images.
     // to do this, we have to add a click listener and check the target
     viewer_element.addEventListener("click", function (event) {
-        if (event.target === viewer_element) {
+        if (event.target.nodeName != "IMG") {
             viewer_element.classList.add("gallery__viewer--hidden")
+            blankViewer()
         }
     });
 });
